d0b219a3dbc4e29582c45d4ce9ea7970377c3e44
Imcab <142848134+Imcab@users.noreply.github.com>
2025-03-19 00:30:08 -0600

    NetworkTablesFix

--- src/main/java/lib/NetworkTableUtils/NormalPublishers/NTData.java
+++ src/main/java/lib/NetworkTableUtils/NormalPublishers/NTData.java
@@ -1,6 +1,5 @@
 package lib.NetworkTableUtils.NormalPublishers;
 
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.util.struct.Struct;
@@ -16,7 +15,5 @@ public class NTData<DataType>{
     public void sendData(DataType value){
         publisher.set(value);
     }
-
-    public static final Struct<Pose3d> Pose3dStruct = Pose3d.struct;
 
 }
--- src/main/java/lib/NetworkTableUtils/Tunnable/Raw/NTTunnable.java
+++
@@ -1,6 +0,0 @@
-package lib.NetworkTableUtils.Tunnable.Raw;
-
-public interface NTTunnable<T> {
-    public T get();
-    public void set(T newValue);
-}
